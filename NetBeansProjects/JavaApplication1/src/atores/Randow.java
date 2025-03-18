378bb557f5ece808fda9bdfa79a67d05b320e3e8
Natália Mendes <162389642+NataliadsMendes@users.noreply.github.com>
2025-03-17 22:30:25 -0300

    Add files via upload

---
+++ NetBeansProjects/JavaApplication1/src/atores/Randow.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atores;
+
+/**
+ *
+ * @author alunolab11
+ */
+class Randow {
+
+    int nexInt() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    int nexInt(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
